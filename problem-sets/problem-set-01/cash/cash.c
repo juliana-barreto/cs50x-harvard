2f4eabec6fa91753f75290af3e33bb240ca78e21
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2025-11-25 19:42:49 -0300

    add pset01 cash

---
+++ problem-sets/problem-set-01/cash/cash.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main (void) {
+  int change = -1;
+  do {
+    printf("\nChange owed (in cents): ");
+    scanf("%d", &change);
+
+    if (change < 0) {
+      printf("Please enter a non-negative amount.\n");
+      continue;
+    }
+
+    //How many quarters are needed
+    int quarters = change / 25;
+    //Update change to reflect remaining cents
+    change = change % 25;
+
+    //How many dimes are needed
+    int dimes = change / 10;
+    change = change % 10;
+
+    //How many nickels are needed
+    int nickels = change / 5;
+    change = change % 5;
+
+    //Remaining cents are pennies
+    int pennies = change;
+
+    //Calculate total coins
+    int totalCoins = quarters + dimes + nickels + pennies;
+
+    printf("Total coins needed: %d\n", totalCoins);
+    return 0;
+  } while (change!= 0);
+}
